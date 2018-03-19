b418fecf407d5cf80e5f4f901d205882b5fda49a
Student P_08 <studentp_08@student-p-08.local>
2018-03-19 11:10:59 +0530

    Initial Commit

---
+++ Urban Cab/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Urban Cab
+//
+//  Created by Student P_08 on 19/03/18.
+//  Copyright © 2018 Pratik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
